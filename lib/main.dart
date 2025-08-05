5043a359b3c426d7d4b15959b1aee765df54fc66
Sean J <seanjanke.bsn@gmail.com>
2025-08-05 09:01:05 +0200

    home page extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/symbols.dart';
-import 'package:responsive_vs_adaptive/adaptive_screen.dart';
-import 'package:responsive_vs_adaptive/responsive_screen.dart';
+import 'package:responsive_vs_adaptive/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,52 +15,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int currentIndex = 0;
-
-  final List<Widget> pages = [ResponsiveDesignPage(), AdaptiveDesignPage()];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          NavigationRail(
-            selectedIndex: currentIndex,
-            backgroundColor: Colors.grey.shade50,
-            indicatorColor: Colors.blue.shade100,
-            onDestinationSelected: (newPageIndex) {
-              setState(() {
-                currentIndex = newPageIndex;
-              });
-            },
-            labelType: NavigationRailLabelType.all,
-            destinations: [
-              NavigationRailDestination(
-                icon: Icon(Symbols.responsive_layout_rounded),
-                label: Text('Responsive'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Symbols.mobile_layout_rounded),
-                label: Text('Adaptive'),
-              ),
-            ],
-          ),
-          VerticalDivider(thickness: 1, width: 1),
-          Expanded(child: pages[currentIndex]),
-        ],
-      ),
     );
   }
 }
